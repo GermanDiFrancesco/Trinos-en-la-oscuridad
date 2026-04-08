739bc2bc513eec0c5b897c446cf44ba4adde2a2a
German <germandifrancesco@gmail.com>
2026-04-08 13:29:44 -0300

    dialog skiping

--- MAN/DialogManager.gd
+++ MAN/DialogManager.gd
@@ -5,13 +5,23 @@ signal dialog_end
 @export var text_container: RichTextLabel 
 @export var options_container: HBoxContainer 
 
+
+# Flag para saber si se está mostrando el texto letra por letra
+var is_showing_text := false
+# Texto completo actual
+var _full_text := ""
+# Referencia al coroutine de display_text
+var _display_text_coroutine = null
+
 #Recibe un texto general y una lista de opciones, cada opción es un diccionario con nombre, funcion y descripcion
 func show_dialog(_text: String = "", options: Array = []) -> void:
 	dialog_start.emit()
 	show()
 	$anim.play("apear")
 	clean_text(options_container)
-	display_text(_text)
+	_full_text = _text
+	is_showing_text = true
+	await display_text(_full_text)
 	options.append({
 		"nombre": "ok",
 		"funcion": "_end_dialog"
@@ -24,18 +34,29 @@ func show_dialog(_text: String = "", options: Array = []) -> void:
 		# Crea un callable con parámetros si existen
 		btn.pressed.connect(Callable(self, option["funcion"]))
 		options_container.add_child(btn)
-	
+
 	await get_tree().process_frame
 	options_container.get_child(0).grab_focus()
 
 func clean_text(container):
 	for child in container.get_children():
 		child.queue_free()
+
+# Muestra el texto letra por letra, puede ser acelerado por input
 func display_text(text: String) -> void:
 	text_container.text = ""
-	for character in text:
-		text_container.text += character
+	for i in text.length():
+		if not is_showing_text:
+			text_container.text = text
+			return
+		text_container.text += text[i]
 		await get_tree().create_timer(0.05).timeout
+	is_showing_text = false
+	
+func _unhandled_input(event):
+	if is_showing_text and (event.is_action_pressed("ui_accept") or event.is_action_pressed("ui_select") or event.is_action_pressed("ui_focus_next")):
+		is_showing_text = false
+
 	
 func _end_dialog() -> void:
 	$anim.play_backwards("apear")
